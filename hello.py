e541b8bdf9a769fa3b3d64ab528fcb6009d9f40d
leonpersi <leonpersik@gmail.com>
2025-03-27 15:41:58 +0200

    Numbers list

--- hello.py
+++ hello.py
@@ -1,16 +1,19 @@
 name = input("What's your name?")
-print("Hello " + name + ", Let's play a game")
+print("Hello " + name + ", Let's play a looping game")
 
-playing = yes
-while playing == yes:
+numbers = []
+
+playing = "yes"
+while playing == "yes":
     number = input("Give me a number from 1 to 10")
     number = int(number)
-
+    numbers.append(number)
     if number <= 5:
         print("Yikes " + name + "." + str(number) + ", that's a small number. Let's triple it to " + str(number * 3) + "!")
     else:
         print("Wow " + name + "." + str(number) + ", that's a big number. Let's double it to " + str(number * 2) + "!")
 
-playing = input("Do you want to play again? (yes/no) : ")
+    playing = input("Do you want to play again? (yes/no) : ")
 
-print("Thank you for playing! " + name + ", Hope we meet again soon. ")
+print("You chose these numbers: " + str(numbers))
+print("Thank you for playing! " + name + ", Hope we meet again soon.")
