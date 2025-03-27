380f61451189ac83cbde9fbc7c7273e43efad176
leonpersi <leonpersik@gmail.com>
2025-03-27 13:33:28 +0200

    hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,9 @@
 name = input("What's your name?")
-print("Hello" + name + ", Hope your'e doing well")
+print("Hello " + name + ", Let's play a game")
+
+number = input("Give me a number from 1 to 10")
+
+number = int(number)
+double = number * 2
+
+print("Cool! " + name + ", Your number when doubled is :" + number + "!")
