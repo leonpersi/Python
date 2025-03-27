552a5a067b91e6c104b32f61aca4bc670d5bab58
leonpersi <leonpersik@gmail.com>
2025-03-27 14:17:56 +0200

    test

--- hello.py
+++ hello.py
@@ -4,6 +4,8 @@ print("Hello " + name + ", Let's play a game")
 number = input("Give me a number from 1 to 10")
 
 number = int(number)
-double = number * 2
 
-print("Cool! " + name + ", Your number when doubled is :" + number + "!")
+if number <= 5:
+    print("Yikes " + name + "." + str(number) + ", that's a small number. Let's triple it to " + str(number * 3) + "!")
+else:
+    print("Wow " + name + "." + str(number) + ", that's a big number. Let's double it to " + str(number * 2) + "!")
