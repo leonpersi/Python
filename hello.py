a0bb81e1fbd66f46bc18ff3de9fdcf9a0ff2ede0
leonpersi <leonpersik@gmail.com>
2025-03-27 14:27:39 +0200

    Playing loop

--- hello.py
+++ hello.py
@@ -1,11 +1,16 @@
 name = input("What's your name?")
 print("Hello " + name + ", Let's play a game")
 
-number = input("Give me a number from 1 to 10")
+playing = yes
+while playing == yes:
+    number = input("Give me a number from 1 to 10")
+    number = int(number)
 
-number = int(number)
+    if number <= 5:
+        print("Yikes " + name + "." + str(number) + ", that's a small number. Let's triple it to " + str(number * 3) + "!")
+    else:
+        print("Wow " + name + "." + str(number) + ", that's a big number. Let's double it to " + str(number * 2) + "!")
 
-if number <= 5:
-    print("Yikes " + name + "." + str(number) + ", that's a small number. Let's triple it to " + str(number * 3) + "!")
-else:
-    print("Wow " + name + "." + str(number) + ", that's a big number. Let's double it to " + str(number * 2) + "!")
+playing = input("Do you want to play again? (yes/no) : ")
+
+print("Thank you for playing! " + name + ", Hope we meet again soon. ")
