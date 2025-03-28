5d48c01c7e7ac122eb929e11e106827f3d288dbc
leonpersi <leonpersik@gmail.com>
2025-03-28 08:30:01 +0300

    round counter + check_number

--- hello.py
+++ hello.py
@@ -1,5 +1,12 @@
 name = input("What's your name?")
 print("Hello " + name + ", Let's play a looping game")
+rounds=0
+def check_number(number):
+    if number <= 5:
+        print("Yikes " + name + "." + str(number) + ", that's a small number. Let's triple it to " + str(number * 3) + "!")
+    else:
+        print("Wow " + name + "." + str(number) + ", that's a big number. Let's double it to " + str(number * 2) + "!")
+
 
 numbers = []
 
@@ -8,12 +15,9 @@ while playing == "yes":
     number = input("Give me a number from 1 to 10")
     number = int(number)
     numbers.append(number)
-    if number <= 5:
-        print("Yikes " + name + "." + str(number) + ", that's a small number. Let's triple it to " + str(number * 3) + "!")
-    else:
-        print("Wow " + name + "." + str(number) + ", that's a big number. Let's double it to " + str(number * 2) + "!")
-
+    rounds=rounds+1
+    check_number(number)
     playing = input("Do you want to play again? (yes/no) : ")
 
-print("You chose these numbers: " + str(numbers))
+print("You chose these numbers: " + str(numbers) + ". You played for:  " + str(rounds) + " rounds!")
 print("Thank you for playing! " + name + ", Hope we meet again soon.")
