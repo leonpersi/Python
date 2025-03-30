65c519dbc2ab0cb9397e3876f18cd9f2fb21af4e
leonpersi <leonpersik@gmail.com>
2025-03-30 16:00:33 +0300

    round for class

--- class.py
+++ class.py
@@ -2,6 +2,7 @@ class Player:
     def __init__(self, name):
         self.name = name
         self.numbers = []
+        self.rounds = 0
         pass
 
     def check_number(self,number):
@@ -18,7 +19,8 @@ player = Player(name)
 print("Hey " + player.name + ", Let's play a game!")
 playing = "yes"
 while playing == "yes":
+    rounds = rounds+1
     number = int(input("Please choose a number between 1 - 10 "))
     player.check_number(number)
     playing = input("Do you want to play again? (yes/no) ")
-print("Thanks for playing, " + player.name + "! You picked: " + str(player.numbers))
+print("Thanks for playing, " + player.name + "! You picked: " + str(player.numbers) + ". You played for: " + str(player.rounds) + "!")
